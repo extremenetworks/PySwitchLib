eeb9a310184132d138458d28a57bf77d5b95920c
Shiv Haris <sharis@brocade.com>
2016-10-20 10:35:10 -0700

    Update version to 0.1.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="pyswitchlib",
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(),
     author="Brocade Comm",
     description="pySwitchLib Library for Brocade switches (SLXOS/NOS)",
