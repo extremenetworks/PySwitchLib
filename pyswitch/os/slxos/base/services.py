86a17b888bf6798f3ba40efe10c0049fb7c422f0
uday <bharatu@brocade.com>
2017-05-20 03:33:01 -0700

    API to enable ISIS

--- pyswitch/os/slxos/base/services.py
+++ pyswitch/os/slxos/base/services.py
@@ -192,3 +192,56 @@ class Services(BaseServices):
 
         config = (vrrpe_method, vrrpe_args)
         return callback(config)
+
+    def isis(self, **kwargs):
+        """Enable or Disable isis.
+        Args:
+            enable (bool): If ISIS should be enabled or disabled.
+                           Default: ``True``.
+            get (bool): Get config instead of editing config. (True, False)
+            callback (function): A function executed upon completion of the
+                method.  The only parameter passed to `callback` will be the
+                ``ElementTree`` `config`.
+
+        Returns:
+            Return value of `callback`.
+
+        Raises:
+            None
+
+        Examples:
+            >>> import pynos.device
+            >>> switches = ['10.24.39.211', '10.24.39.203']
+            >>> auth = ('admin', 'password')
+            >>> for switch in switches:
+            ...     conn = (switch, '22')
+            ...     with pynos.device.Device(conn=conn, auth=auth) as dev:
+            ...         dev.services.isis(get=True)
+            ...         dev.services.isis(enable=False)
+            ...         dev.services.isis()
+            Traceback (most recent call last):
+            KeyError
+        """
+        enable = kwargs.pop('enable', True)
+
+        get = kwargs.pop('get', False)
+
+        callback = kwargs.pop('callback', self._callback)
+
+        if get:
+            enable = None
+
+        isis_args = {}
+        if get:
+            config = ('router_isis_get', isis_args)
+            x = callback(config, handler='get_config')
+            util = Util(x.data)
+            result = util.find(util.root, './/isis//auth-check')
+            print('dddddddd', result)
+            return result
+        if not enable:
+            config = ('router_isis_delete', isis_args)
+        else:
+            config = ('router_isis_create', isis_args)
+
+        return callback(config)
