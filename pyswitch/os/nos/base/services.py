8441777be22ff5505817547b93c330038546886d
Samir Savla <samirsavla@gmail.com>
2017-09-20 14:50:10 -0700

    Fixing pylint issues pass #1

--- pyswitch/os/nos/base/services.py
+++ pyswitch/os/nos/base/services.py
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import xml.etree.ElementTree as ET
-from pyswitch.utilities import Util
 from pyswitch.os.base.services import Services as BaseServices
+from pyswitch.utilities import Util
+
 
 class Services(BaseServices):
     """
@@ -52,14 +52,14 @@ class Services(BaseServices):
         util = Util(results.data)
         result = []
 
-        for item in util.findlist(util.root,'.//arp-entry' ):
-            ip_address = util.find(item,'.//ip-address' )
-            mac_address = util.find(item,'.//mac-address' )
-            interface_type = util.find(item,'.//interface-type' )
-            interface_name = util.find(item,'.//interface-name' )
-            is_resolved = util.find(item,'.//is-resolved' )
-            age = util.find(item,'.//age' )
-            entry_type = util.find(item,'.//entry-type' )
+        for item in util.findlist(util.root, './/arp-entry'):
+            ip_address = util.find(item, './/ip-address')
+            mac_address = util.find(item, './/mac-address')
+            interface_type = util.find(item, './/interface-type')
+            interface_name = util.find(item, './/interface-name')
+            is_resolved = util.find(item, './/is-resolved')
+            age = util.find(item, './/age')
+            entry_type = util.find(item, './/entry-type')
             item_results = {'ip-address': ip_address,
                             'mac-address': mac_address,
                             'interface-type': interface_type,
