57b4cad6b339cf6f1f6f1c03f5e63e759321252f
m-selvam <m.selvaa@gmail.com>
2017-09-29 21:16:30 +0000

    phy interface admin enable/disable set/get

--- pyswitch/snmp/mlx/base/interface.py
+++ pyswitch/snmp/mlx/base/interface.py
@@ -106,3 +106,88 @@ class Interface(BaseInterface):
             reason = error.message
             raise ValueError('Failed to create VLAN due to %s', reason)
             return False
+
+    def admin_state(self, **kwargs):
+        """Set interface administrative state.
+
+        Args:
+            int_type (str): Type of interface. (ethernet, etc).
+            name (str): Name of interface. (1/1, etc).
+            enabled (bool): Is the interface enabled? (True, False)
+            get (bool): Get config instead of editing config. (True, False)
+            callback (function): A function executed upon completion of the
+                method.  The only parameter passed to `callback` will be the
+                ``ElementTree`` `config`.
+
+        Returns:
+            Return value of `callback`.
+
+        Raises:
+            KeyError: if `int_type`, `name`, or `enabled` is not passed and
+                `get` is not ``True``.
+            ValueError: if `int_type`, `name`, or `enabled` are invalid.
+
+        Examples:
+            >>> import pyswitch.device
+            >>> switches = ['10.24.39.211', '10.24.39.203']
+            >>> auth = ('admin', 'password')
+            >>> for switch in switches:
+            ...     conn = (switch, '22')
+            ...     with pyswitch.device.Device(conn=conn, auth=auth) as dev:
+            ...         dev.interface.admin_state(
+            ...         int_type='ethernet', name='1/1',
+            ...         enabled=False)
+            ...         dev.interface.admin_state(
+            ...         int_type='ethernet', name='1/1',
+            ...         enabled=True)
+        """
+
+        int_type = kwargs.pop('int_type').lower()
+        name = kwargs.pop('name')
+        get = kwargs.pop('get', False)
+        if get:
+            enabled = None
+        else:
+            enabled = kwargs.pop('enabled')
+        port_id = kwargs.pop('port_id', None)
+
+        callback = kwargs.pop('callback', self._callback)
+        valid_int_types = self.valid_int_types
+        ifAdminStatus_oid ="1.3.6.1.2.1.2.2.1.7"
+        ifadminStatus_index = ifAdminStatus_oid + '.' + `port_id`
+
+        if int_type == 'ethernet' and port_id is None:
+            raise ValueError('pass valid port-id')
+
+        if int_type not in valid_int_types:
+            raise ValueError('`int_type` must be one of: %s' %
+                             repr(valid_int_types))
+
+        if not isinstance(enabled, bool) and not get:
+            raise ValueError('`enabled` must be `True` or `False`.')
+
+        try:
+            if get:
+                value = callback(ifadminStatus_index)
+
+                if value == 1:
+                    enabled = True
+                else:
+                    enabled = False
+                return enabled
+            else:
+                if enabled:
+                    value = 1 #enable
+                else:
+                    value = 2 #disable
+                  
+                state_args = (ifadminStatus_index, value)
+
+                return callback(state_args, handler='snmp-set')
+        except AttributeError:
+            return None
+        except Exception as error:
+            reason = error.message
+            raise ValueError('Failed to set interface admin status to %s',
+                reason)
+        return None
