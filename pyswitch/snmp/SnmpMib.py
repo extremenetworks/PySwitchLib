06c63b28778945f4849e0f1f5c843cc408745c98
dpasupathi <pasu@brocade.com>
2017-10-02 15:05:13 -0700

    Pin version on requirements file. Also fixed minor review comments.

--- pyswitch/snmp/SnmpMib.py
+++ pyswitch/snmp/SnmpMib.py
@@ -2,6 +2,7 @@
 
 class SnmpMib:
     mib_oid_map = {
+        'sysObjectId': '1.3.6.1.2.1.1.2.0',
         'dot1qVlanStaticTable': '1.3.6.1.2.1.17.7.1.4.3.1',
         'dot1qVlanStaticEntry': '1.3.6.1.2.1.17.7.1.4.3.1.1',
         'dot1qVlanStaticName': '1.3.6.1.2.1.17.7.1.4.3.1.2',
