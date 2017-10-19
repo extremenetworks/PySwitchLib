fdde53d2b272f7c67cfe4558d685f09b5acd5464
m-selvam <m.selvaa@gmail.com>
2017-10-19 01:11:04 +0000

    global IP MTU config for both ipv4 and ipv6

--- pyswitch/snmp/mlx/base/system.py
+++ pyswitch/snmp/mlx/base/system.py
@@ -85,3 +85,70 @@ class System(BaseSystem):
             success_msg = re.search(r'Reload required.(.+)', cli_res)
 
         return success_msg.group(0)
+
+    def system_ip_mtu(self, **kwargs):
+        """Set system mtu.
+
+            Args:
+
+                mtu (str): Value between 576  and 9198 for ipv4
+                           value between 1280 - 8982 for ipv6
+                version (int) : 4 or 6
+                callback (function): A function executed upon completion of
+                    the method.
+
+            Returns:
+                Return value of True.
+
+            Raises:
+                ValueError:  `mtu` is invalid.
+
+            Examples:
+                >>> import pyswitch.device
+                >>> switches = ['10.24.85.107']
+                >>> auth = ('admin', 'admin')
+                >>> for switch in switches:
+                ...  conn = (switch, '22')
+                ...  with pyswitch.device.Device(conn=conn, auth=auth) as dev:
+                ...         output = dev.system.system_ip_mtu(mtu='1666')
+                ...         output = dev.system.system_ip_mtu(mtu='1667',version=6)
+            """
+
+        callback = kwargs.pop('callback', self._callback)
+        version = kwargs.pop('version', 4)
+
+        mtu = kwargs.pop('mtu')
+
+        cli_arr = []
+
+        if version is 4:
+            minimum_mtu = 576
+            maximum_mtu = 9198
+            if int(mtu) < minimum_mtu or int(mtu) > maximum_mtu:
+                raise ValueError(
+                    "Incorrect mtu value, Valid Range %s-%s" %
+                    (minimum_mtu, maximum_mtu))
+            cli_arr.append('ip global-mtu' + ' ' + str(mtu))
+
+        if version is 6:
+            minimum_mtu = 1280
+            maximum_mtu = 8982
+            if int(mtu) < minimum_mtu or int(mtu) > maximum_mtu:
+                raise ValueError(
+                    "Incorrect mtu value, Valid Range %s-%s" %
+                    (minimum_mtu, maximum_mtu))
+
+            cli_arr.append('ipv6 global-mtu' + ' ' + str(mtu))
+
+        try:
+            cli_res = callback(cli_arr, handler='cli-set')
+            error = re.search(r'Error:(.+)', cli_res)
+            invalid_input = re.search(r'Invalid input', cli_res)
+            if error:
+                raise ValueError("%s" % error.group(0))
+            if invalid_input:
+                raise ValueError("%s" % invalid_input.group(0))
+            return True
+        except Exception as error:
+            reason = error.message
+            raise ValueError('Failed to add member port to vlan %s' % (reason))
