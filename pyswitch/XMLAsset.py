33f6c05082e8c510c070a0d7ffabe6cc35c189c5
badaniya <badaniya@brocade.com>
2017-11-10 09:17:31 -0800

    Fixed formatting.

--- pyswitch/XMLAsset.py
+++ pyswitch/XMLAsset.py
@@ -5,8 +5,10 @@ from pyswitchlib.asset import Asset
 
 
 class XMLAsset(Asset):
-    def __init__(self, ip_addr='', auth=('admin', 'password'), rest_proto='', ca_cert='', fw_ver='', timeout=''):
-        super(XMLAsset, self).__init__(ip_addr=ip_addr, auth=auth, rest_proto=rest_proto, ca_cert=ca_cert, fw_ver=fw_ver, timeout=timeout)
+    def __init__(self, ip_addr='', auth=('admin', 'password'), 
+                rest_proto='', ca_cert='', fw_ver='', timeout=''):
+        super(XMLAsset, self).__init__(ip_addr=ip_addr, auth=auth, 
+                rest_proto=rest_proto, ca_cert=ca_cert, fw_ver=fw_ver, timeout=timeout)
 
     def _rest_operation(self, rest_commands=None, yang_list=None, timeout=None):
         auth = self._auth
@@ -37,7 +39,7 @@ class XMLAsset(Asset):
                 uri_prefix_path = self._rest_discover_path
 
             header = {"Resource-Depth": str(rest_cmd[4])}
-            url = self._rest_protocol+"://" + self._ip_addr + uri_prefix_path
+            url = self._rest_protocol + "://" + self._ip_addr + uri_prefix_path
 
             self._session.headers.update(
                 {'Content-type': 'application/x-www-form-urlencoded'})
