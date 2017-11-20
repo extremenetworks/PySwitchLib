fed58e3aafc1b3b1ada0ed069957f99bc9f890a8
vivekbrcd <svivek@brocade.com>
2017-11-20 21:10:45 +0530

    flake8 fix

--- pyswitch/snmp/base/acl/acl.py
+++ pyswitch/snmp/base/acl/acl.py
@@ -378,7 +378,7 @@ class Acl(object):
 
     def _is_parameter_supported(self, supported_params, parameters):
 
-        received_params = [k for k, v in parameters.iteritems() if not v]
+        received_params = [k for k, v in parameters.iteritems() if v]
         unsupported_params = list(set(received_params) - set(supported_params))
         if len(unsupported_params) > 0:
             raise ValueError("unsupported parameters provided: {}"
