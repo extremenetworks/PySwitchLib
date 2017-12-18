8131374ecef2614dae6e85667fb6469bc2b5f1b3
Ubuntu <ubuntu@st2vagrant>
2017-12-18 10:09:11 +0000

    Fix for defects MLX-223,MLX-228,MLX-248

--- pyswitch/snmp/SnmpMib.py
+++ pyswitch/snmp/SnmpMib.py
@@ -6,9 +6,9 @@ class SnmpMib:
     """
     mib_oid_map = {
         'sysObjectId': '1.3.6.1.2.1.1.2.0',
-        'dot1qVlanStaticTable': '1.3.6.1.2.1.17.7.1.4.3.1',
-        'dot1qVlanStaticEntry': '1.3.6.1.2.1.17.7.1.4.3.1.1',
-        'dot1qVlanStaticName': '1.3.6.1.2.1.17.7.1.4.3.1.2',
+        'dot1qVlanStaticTable': '1.3.6.1.2.1.17.7.1.4.3',
+        'dot1qVlanStaticEntry': '1.3.6.1.2.1.17.7.1.4.3.1',
+        'dot1qVlanStaticName': '1.3.6.1.2.1.17.7.1.4.3.1.1',
         'dot1qVlanStaticEgressPorts': '1.3.6.1.2.1.17.7.1.4.3.1.2',
         'dot1qVlanStaticUntaggedPorts': '1.3.6.1.2.1.17.7.1.4.3.1.4',
         'dot1qVlanStaticRowStatus': '1.3.6.1.2.1.17.7.1.4.3.1.5',
